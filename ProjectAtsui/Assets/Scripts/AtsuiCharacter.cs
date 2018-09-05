06f20d137daa74e03a55828d8d2da5dba74f9f90
poikelp <takukelp@gmail.com>
2018-09-05 19:17:59 +0900

    class名間違えてた

--- ProjectAtsui/Assets/Scripts/AtsuiCharacter.cs
+++ ProjectAtsui/Assets/Scripts/AtsuiCharacter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class AtsuiCharacter : MonoBehaviour {
 
 	[SerializeField]
 	private float growSpeed;
