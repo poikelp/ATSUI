d74bfc7d00ac63dd2fbd0b1ff4a3b4ce3d7b1e07
poikelp <takukelp@gmail.com>
2018-09-07 21:55:26 +0900

    各メソッドの説明コメントを追加

--- ProjectAtsui/Assets/Scripts/AtsuiCharacter.cs
+++ ProjectAtsui/Assets/Scripts/AtsuiCharacter.cs
@@ -18,25 +18,30 @@ public class AtsuiCharacter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GrowUpCharacter();
+		GrowUpCharacter());
 		ScaleUpdate();
 	}
 
+	//キャラクターを決められた値だけ大きくする
 	void GrowUpCharacter () {
 		scale_character.x += growSpeed;
 		scale_character.z += growSpeed;
 	}
 
+	//キャラクターの大きさを更新する
 	void ScaleUpdate () {
 		transform_character.localScale = scale_character;
 	}
 
+	//キャラクターを決められた値だけ小さくする
 	void GrowDownCharacter () {
 		scale_character.x -= growSpeed * 2;
 		scale_character.z -= growSpeed * 2;
-
+		
+		SizeCheck();
 	}
 
+	//キャラクターの大きさがマイナスにならないように修正する
 	void SizeCheck () {
 		if(scale_character.x < 0)
 			scale_character.x = 0;
