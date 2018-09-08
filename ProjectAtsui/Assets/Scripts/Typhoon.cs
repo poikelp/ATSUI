b96a7b8cd5898bb032efc6d09010ead47753c69a
poikelp <takukelp@gmail.com>
2018-09-09 01:43:17 +0900

    台風が北上するようになった

--- ProjectAtsui/Assets/Scripts/Typhoon.cs
+++ ProjectAtsui/Assets/Scripts/Typhoon.cs
@@ -8,8 +8,11 @@ public class Typhoon : MonoBehaviour {
 	float growSpeed;
 	[SerializeField]
 	float spinSpeed;
+	[SerializeField]
+	float goNothSpeed;
 
 	Transform transform_this;
+	Vector3 velocity;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +25,12 @@ public class Typhoon : MonoBehaviour {
 		GrowTyphoon();
 	}
 
+	void FixedUpdate () {
+		ResetVelocity();
+		GoingNorth();
+		PositionUpdate();
+	}
+
 	void GrowTyphoon () {
 		transform_this.localScale = transform_this.localScale * (1 + growSpeed * Time.deltaTime);
 	}
@@ -29,5 +38,17 @@ public class Typhoon : MonoBehaviour {
 	void SpinTyphoon () {
 		Vector3 torque = new Vector3(0, 0, spinSpeed * Time.deltaTime);
 		transform_this.Rotate(torque);
+	}
+
+	void PositionUpdate (){
+		transform_this.position = transform_this.position + velocity;
+	}
+
+	void ResetVelocity () {
+		velocity = Vector3.zero;
+	}
+
+	void GoingNorth () {
+		velocity.y += goNothSpeed * Time.deltaTime;
 	}
 }
