f3a84ac8035be7f23fa5bf7a44b6a583b89d2a9a
poikelp <takukelp@gmail.com>
2018-09-09 02:36:20 +0900

    偏西風参戦！！

---
+++ ProjectAtsui/Assets/Scripts/Westerlies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Westerlies : MonoBehaviour {
+
+	Vector3 newPos;
+	Transform transform_this;
+
+	// Use this for initialization
+	void Start () {
+		newPos = new Vector3(0.0f, 7.0f, 0.0f);
+		transform_this = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		MoveWesterlies();
+	}
+
+	void MoveWesterlies () {
+		newPos.y = 7.0f + GetSinTime() * 5;
+		transform_this.position = newPos;
+	}
+
+	float GetSinTime () {
+		return Mathf.Sin(Time.time);
+	}
+}
