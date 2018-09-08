17cc39a8f5becfe2f1044bd41735797d8db692e4
poikelp <takukelp@gmail.com>
2018-09-09 01:32:14 +0900

    台風が回転するようになった

--- ProjectAtsui/Assets/Scripts/Typhoon.cs
+++ ProjectAtsui/Assets/Scripts/Typhoon.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Typhoon : MonoBehaviour {
 
+	[SerializeField]
+	float growSpeed;
+	[SerializeField]
+	float spinSpeed;
+
+	Transform transform_this;
+
 	// Use this for initialization
 	void Start () {
-		
+		transform_this = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SpinTyphoon();
+		GrowTyphoon();
+	}
+
+	void GrowTyphoon () {
+		transform_this.localScale = transform_this.localScale * (1 + growSpeed * Time.deltaTime);
+	}
+
+	void SpinTyphoon () {
+		Vector3 torque = new Vector3(0, 0, spinSpeed * Time.deltaTime);
+		transform_this.Rotate(torque);
 	}
 }
