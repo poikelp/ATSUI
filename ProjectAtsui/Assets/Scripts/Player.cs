bdd1a48e8e7149f67dc72ea48f9fd11fa3485f95
poikelp <takukelp@gmail.com>
2018-09-09 00:56:17 +0900

    クリックした座標にｔｙｐほおんwo

--- ProjectAtsui/Assets/Scripts/Player.cs
+++ ProjectAtsui/Assets/Scripts/Player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	GameObject typhoon;
+
 	// Use this for initialization
 	void Start () {
-		
+		typhoon = (GameObject)Resources.Load("Prefab/typhoon");
 	}
 	
 	// Update is called once per frame
@@ -15,6 +17,9 @@ public class Player : MonoBehaviour {
 	}
 
 	public void MakeTyphoon (){
-		
+		Vector2 mousePos = Input.mousePosition;
+		Vector3 spownPos = new Vector3(mousePos.x, mousePos.y, 0);
+
+		Instantiate(typhoon, spownPos, Quaternion.identity);
 	}
 }
